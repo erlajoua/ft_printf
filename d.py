eb5c35234212e6bacaaad99300f627900c91e706
Erwan Lajouannique <er.lajouannique@gmail.com>
2020-10-30 11:12:25 +0100

    ajout du testeur

--- d.py
+++ d.py
@@ -6,12 +6,12 @@ print('#include "ft_printf.h"');
 print("")
 
 print("int main(void)\n{", end='')
-for i in range(9): #NOMBRE DE TESTS A GENERER ICI
+for i in range(25): #NOMBRE DE TESTS A GENERER ICI
     bool_minus = random.randint(0,1)
     bool_double_arg = random.randint(0,1)
     bool_first_arg = random.randint(0,1)
     bool_second_arg = random.randint(0,1)
-    print("\n\tft_printf(\"-->|%",end='')
+    print("\n\tprintf(\" <- [%d]\\n\", ft_printf(\"|%",end='')
     if bool_minus != 0:
         print("-", end='')
     if bool_double_arg != 0:
@@ -27,7 +27,7 @@ for i in range(9): #NOMBRE DE TESTS A GENERER ICI
         else:
             random2 = random.randint(0,20)
             print(random2, end='')
-        print("d|\\n\"", end='')
+        print("d|\"", end='')
         if bool_first_arg != 0:
             print(", ", end='')
             random3 = random.randint(-20,20)
@@ -39,19 +39,19 @@ for i in range(9): #NOMBRE DE TESTS A GENERER ICI
     else:
         bool_single_arg = random.randint(0,1)
         if bool_single_arg != 0:
-            print("*d|\\n\", ", end='')
+            print("*d|\", ", end='')
             random5 = random.randint(-20,20)
             print(random5, end='')
         else:
             random6 = random.randint(20,20)
             if random6 != 0:
                 print(random6, end='')
-            print("d|\\n\"", end='')
+            print("d|\"", end='')
     random7 = random.randint(-20,20)
     print(", ", end='')
     print(random7, end='')
-    print(");")
-    print("\tprintf(\"-->|%",end='')
+    print("));")
+    print("\tprintf(\" <- [%d]\\n\\n\", printf(\"|%",end='')
     if bool_minus != 0:
         print("-", end='')
     if bool_double_arg != 0:
@@ -65,7 +65,7 @@ for i in range(9): #NOMBRE DE TESTS A GENERER ICI
             print("*", end='')
         else:
             print(random2, end='')
-        print("d|\\n\\n\"", end='')
+        print("d|\"", end='')
         if bool_first_arg != 0:
             print(", ", end='')
             print(random3, end='')
@@ -74,13 +74,13 @@ for i in range(9): #NOMBRE DE TESTS A GENERER ICI
             print(random4, end='')
     else:
         if bool_single_arg != 0:
-            print("*d|\\n\\n\", ", end='')
+            print("*d|\", ", end='')
             print(random5, end='')
         else:
             if random6 != 0:
                 print(random6, end='')
-            print("d|\\n\\n\"", end='')
+            print("d|\"", end='')
     print(", ", end='')
     print(random7, end='')
-    print(");")
+    print("));")
 print("\n\treturn (0);\n}")
