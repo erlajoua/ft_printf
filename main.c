2a9de149421d311b7dafaba20a27d4bed5742fdc
Erwan Lajouannique <er.lajouannique@gmail.com>
2020-11-03 11:50:51 +0100

    second commit %i

--- main.c
+++ main.c
@@ -2,18 +2,11 @@
 
 int main(void)
 {
-	int nb = -44564564;
+	int nb = 135;
 
-	printf("%i\n", ft_printf("|%-15.*i|", 7, nb));
-	printf("%i\n\n", printf("|%-15.*i|", 7, nb));
-	
-
-	printf("%i\n", ft_printf("|%-7.*i|", -9, nb));
-	printf("%i\n\n", printf("|%-7.*i|", -9, nb));
-
-	printf("%i\n", ft_printf("|%-7.*i|", -15, nb));
-	printf("%i\n\n", printf("|%-7.*i|", -15, nb));
-	
+	printf("%d\n", ft_printf("|%-4.4i|", nb));
+	printf("%d\n\n", printf("|%-4.4i|", nb));
+		
 	
 	return (0);
 
