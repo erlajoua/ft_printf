2c299e8dbfbada0be2899c4128015b1bab97fe71
Erwan Lajouannique <er.lajouannique@gmail.com>
2020-11-05 14:01:10 +0100

    main.c

--- main.c
+++ main.c
@@ -2,14 +2,13 @@
 
 int main(void)
 {
-		printf("%d\n", ft_printf("|%0X|", 42));
-		printf("%d\n", ft_printf("|%0x|", 42));
-		printf("%d\n", ft_printf("|%0d|", 42));
-		printf("%d\n", ft_printf("|%0i|", 42));
-		printf("%d\n", ft_printf("|%0c|", 's'));
-		printf("%d\n", ft_printf("|%0s|", "salut"));
-		
-	return (0);
-
+	printf("-->|%-4.%|<--\n");	
+	printf("-->|%-4.*%|<--\n", -4);	
+	printf("-->|%-4.*%|<--\n", -3);	
+	printf("-->|%-4.*%|<--\n", -2);	
+	printf("-->|%-4.*%|<--\n", -1);	
+	printf("-->|%-4.*%|<--\n", 0);	
+	printf("-->|%-4.*%|<--\n", 1);	
+	printf("-->|%-4.*%|<--\n", 2);	
+	return 0;
 }
-
